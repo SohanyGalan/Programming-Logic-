a5931b7fb5f1bf0596b078a6e355d8ef8ec24904
Sohany Galan Garcia <sohanygalan@gmail.com>
2024-10-14 23:23:24 -0400

    Create operadores_logicos.java

---
+++ operadores_logicos.java
@@ -0,0 +1,16 @@
+class Operadoreslogicos{
+    public static void main(String[] args) {
+        
+        boolean v = true;
+        boolean f = false; 
+        
+        System.out.println("v && f = " + (v&&f));
+        System.out.println("v || f = " + (v||f));
+        System.out.println(" !(v && f ) = " + ! (v && f));
+        
+//&& (AND lógico): Ambas condiciones deben ser verdaderas.
+//|| (OR lógico): Al menos una condición debe ser verdadera.
+//! (NOT lógico): Invierte el valor booleano.
+//^ (XOR lógico): Verdadero si una de las dos es verdadera, pero no ambas.
+    }
+}
