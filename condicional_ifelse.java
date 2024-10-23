903a5fd3e296ee175323fce2e3b15ca711a847e2
Sohany Galan Garcia <sohanygalan@gmail.com>
2024-10-22 23:51:37 -0400

    Add files via upload

---
+++ condicional_ifelse.java
@@ -0,0 +1,13 @@
+public class condicionales {
+    public static void main(string[] args) 
+}
+
+int numero = 5; //aqui le estamos asignando un valor a la variable 'numero'
+
+if (numero > 0 ) { //si numero es mayor que 0, entonces ejecutame este bloque 
+System.out.println("El numero es positivo");
+
+} else if (numero < 0 ) {
+System
+}
+
