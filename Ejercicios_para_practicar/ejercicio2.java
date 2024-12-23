83483f1e440bd6c71b9977f945cb216f201d89ba
Sohany Galan <sohanygalan@gmail.com>
2024-12-22 23:45:36 -0400

    new exercise

---
+++ Ejercicios_para_practicar/ejercicio2.java
@@ -0,0 +1,30 @@
+/*
+        Ejercicio 2:
+        Conversión de Monedas
+        Crea un programa que convierta de dólares a pesos dominicanos usando una constante para la tasa de cambio.
+
+        Instrucciones:
+        1. Define una constante TASA_DE_CAMBIO (por ejemplo, 1 USD = 56.5 DOP).
+        2. Solicita al usuario la cantidad en dólares.
+        3. Calcula y muestra el equivalente en pesos.
+        */
+
+        package Ejercicios_para_practicar;
+
+import java.util.Scanner;
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final double TASA_DE_CAMBIO = 56.5;
+
+        System.out.println("Introduce la cantidad en dólares: ");
+        double dolares = scanner.nextDouble();
+
+        double pesos = dolares * TASA_DE_CAMBIO;
+
+        System.out.println("La cantidad en pesos es: " + pesos);
+        scanner.close();
+    }
+}
---
+++ Ejercicios_para_practicar/ejercicio3.java
@@ -0,0 +1,34 @@
+/*
+Ejercicio 3: Tabla de Valores IVA
+Crea un programa que calcule el precio final de un producto con impuestos usando una constante para la tasa de IVA.
+
+Instrucciones:
+1. Define IVA como una constante (por ejemplo, 0.18 para un IVA del 18%).
+2. Solicita al usuario el precio de un producto.
+3. Calcula y muestra:
+   - El monto del IVA.
+   - El precio final (precio + IVA).
+*/
+
+package Ejercicios_para_practicar;
+
+import java.util.Scanner;
+
+public class ejercicio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final double iva = 0.18;
+
+        System.out.println("Ingrese el precio del producto:");
+        double precio = scanner.nextDouble();
+
+        double ivaTotal = precio * iva;
+        double precioFinal = precio + ivaTotal;
+
+        System.out.println("El precio final es: " + precioFinal);
+        System.out.println("El monto del IVA es: " + ivaTotal);
+        scanner.close();
+
+    }
+}
